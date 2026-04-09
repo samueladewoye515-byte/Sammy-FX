a2739d1e3d13539a8d7ff2ec029d1e3862077f5d
Promise Akpabio <enwongoakpabio01@gmail.com>
2026-04-09 15:14:08 +0100

    made some changes

--- smart_chatbot.py
+++ smart_chatbot.py
@@ -418,4 +418,4 @@ if __name__ == '__main__':
     print("\n⚠️  Press Ctrl+C to stop the server")
     print("="*60 + "\n")
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=8000)
